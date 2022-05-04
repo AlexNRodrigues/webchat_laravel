e9f1f54b10eb8c076073616ded6a8769e0ab9111
AlexNRodrigues <alexnasscimento99@gmail.com>
2022-05-04 10:07:53 -0300

    first commit

---
+++ resources/js/store.js
@@ -0,0 +1,27 @@
+import axios from "axios"
+import Vue from "vue"
+import Vuex from "vuex"
+import cretePersistedState from 'vuex-persistedstate'
+
+// Vue.useAttrs(Vuex);
+
+export default new Vuex.Store({
+    state: {
+        user: {}
+    },
+    mutations: {
+        setUserState: (state, value) => state.user = value
+    },
+    actions: {
+        userStateAction: ({commit :commit}) => {
+            axios.get('api/user/me').then(response => {
+                const userResponse = response.data.user
+                commit('setUserState', userResponse)
+            })
+        }
+    },
+    plugins: [
+        cretePersistedState()
+    ]   
+
+})
